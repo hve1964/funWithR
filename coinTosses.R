868d39cfd4c58acf7d1621392672257126ce0b80
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2021-05-30 15:31:11 +0200

    Add files via upload

---
+++ coinTosses.R
@@ -0,0 +1,37 @@
+################################################################################
+# The Number of Times a Coin Needs to Be Flipped Until Head Appears
+#   URL: https://math.stackexchange.com/questions/2738638/the-number-of-times-a-coin-needs-to-be-flipped-until-head-appears
+# Expected value of the number of flips until the first head
+#   URL: https://math.stackexchange.com/questions/1196452/expected-value-of-the-number-of-flips-until-the-first-head
+# Date: So, 30.05.2021
+################################################################################
+
+# Specify probability for success
+prob_for_success <- 0.5
+
+# Specify number of trials
+number_of_trials <- 1e6
+
+tosses_til_head <- 0 # An empty vector
+
+for(sim in 1:number_of_trials) {
+  # Number of simulations: ...
+
+  i <- 1 # Counter
+  repeat {
+    if (rbinom(n = 1, size = 1, prob = prob_for_success) == 0) {
+      # Coin toss with Boolean (if tails...)
+      i <- i + 1
+    } # ... increase the counter by 1.
+    else {
+      break
+    } # If heads, stop!
+  }
+  
+  tosses_til_head[sim] <- i # Collect the number of tosses before we got "Head".
+}
+
+mean(x = tosses_til_head) # Average of these ... simulations.
+
+################################################################################
+################################################################################
