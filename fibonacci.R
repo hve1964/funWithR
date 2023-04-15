754e926ee0155aecba66e96b39bb84399b078c02
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2023-04-15 22:32:07 +0200

    Add files via upload

---
+++ fibonacci.R
@@ -0,0 +1,20 @@
+fibonacci <-
+  function(n) {
+    if (n < 3) {
+      stop(paste0("Fehler!\n",
+                  "Gib einen Wert fuer n groesser oder gleich 3 ein."))
+    }
+    
+    folge <-
+      integer(length = n)
+    
+    folge[1:2] <-
+      c(1L, 1L)
+    
+    for (i in 3:n) {
+      folge[i] <-
+        folge[i - 1] + folge[i - 2]
+    }
+    
+    return(folge)
+  }
